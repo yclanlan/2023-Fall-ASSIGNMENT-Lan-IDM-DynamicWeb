705d78c2b3cd795f77bf7a99478161d8d2b1d287
yclanlan <lanlan.yichun@gmail.com>
2023-09-06 16:25:33 -0400

    add content of recipt

--- Week1/hellooooreact/src/index.js
+++ Week1/hellooooreact/src/index.js
@@ -29,7 +29,34 @@ function App(){
 
   return (
     <>
+    <h1> Fried Garlic Water Spinach </h1>
+    <img src="https://img.soundofhope.org/2020-07/88-1596049315262.jpg"
+    style={{ width:'50%'}}></img>
 
+    <h3>Ingredients</h3>
+    <li>water spinach </li>
+      <li>garlic</li>
+      <li>chili</li>
+      <li>salt</li>
+    <ul> 
+      
+    </ul>
+
+    <h3>step</h3>
+    <ol> 
+
+
+      <li>After applying a coat of oil to the pan, add sliced garlic and chili. Sauté until they become tender and fragrant. </li>
+      <li>Once the garlic and chili are ready, introduce the vegetable stalks into the pan. Sprinkle with salt.</li>
+      <li>Cover the pan with a lid and allow the vegetables to steam briefly. The leaves will release their moisture as they cook.</li>
+      <li>When everything is cooked to perfection, it's time to plate up and enjoy!</li>
+    </ol>
+
+
+
+    <br/><br/><br/><br/><br/><br/>
+   {/* ---------- what we learned in class ---------- */}
+  <hr/>
     <h1> Hello React!</h1>
       <p> My name is {name} and I am {age} years old</p>
       <p> flip: {flip}</p>
@@ -43,7 +70,9 @@ function App(){
       <hr/>
 
       <p> Print the array out: {arr} </p>
-      {/* <p> {myObj} </p> */}
+
+      { /* <p> {myObj} </p> */ }
+
       <button disable={false} style={{color:'red'}}> Click Me </button>
       <input type="number" min={5} />
 
