0a07d1e62de344bafaab9e2e10d58e511bedad80
yclanlan <lanlan.yichun@gmail.com>
2023-09-20 15:04:39 -0400

    copy week 2 as base of week 3

--- Week03/test-project/src/Card.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-// import './styles.css'
-import styles from './RecipeCard.module.css'
-// Hmmm. This looks like a reusable UI component...
-// Lets discuss next class
-export default function Card(props) {
-  return <div className={styles.card}>{props.children}</div>
-}
